1952611dcf25b3705140f60b07945459a9d7f089
Bruno Teixeira <brunomiguelteix@hotmail.com>
2024-01-07 19:22:31 +0000

    improve modal

--- src/AnniversaryPopup.js
+++ src/AnniversaryPopup.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
-import { LuPartyPopper } from "react-icons/lu";
 
 const AnniversaryPopup = ({ show, handleClose }) => {
+    const anivImg = `${process.env.PUBLIC_URL}/IMG_8738.JPG`;
 
     return (
-        <Modal show={show} onHide={handleClose} centered >
-            <Modal.Header closeButton className="anniversary-header text-center">
-                <Modal.Title className="anniversary-title">40º Aniversário</Modal.Title>
+        <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton className="anniversary-header">
+                <Modal.Title><a href="#40anos" className="anniversary-title">40º Aniversário</a></Modal.Title>
             </Modal.Header>
             <Modal.Body className="anniversary-body">
-                <p className="anniversary-text">
+                <div className="texto-aniv-pop">
                     Estamos a celebrar quatro décadas de inovação e sucesso. Obrigado por fazer parte desta jornada!
-                </p>
-                <LuPartyPopper size={60}></LuPartyPopper>
+                </div>
+
+                <div className="imagem-aniv-pop">
+                    <a href="#40anos"><img src={anivImg} alt="aniv" loading="lazy" /></a>
+                </div>
             </Modal.Body>
         </Modal >
     );
